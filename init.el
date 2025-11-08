c97b1fc79bb0e9259691b7798d2f9270c6e76a10
Ralfs Kristians Vētra <ralfskristiansvetra@gmail.com>
2025-11-08 17:23:12 +0200

    Refactor init.el for improved organization and clarity

--- init.el
+++ init.el
@@ -1,7 +1,11 @@
+(setq custom-file (concat user-emacs-directory "custom.el"))
+(when (file-exists-p custom-file)
+  (load custom-file))
+
 (add-to-list 'default-frame-alist '(fullscreen . maximized))
 
 (setq ring-bell-function 'ignore)
-
+(setq display-line-numbers-type 'relative)
 (setq inhibit-startup-screen t
       inhibit-startup-echo-area-message user-login-name
       initial-scratch-message nil)
@@ -9,47 +13,39 @@
 (menu-bar-mode -1)
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
-
 (column-number-mode 1)
 (show-paren-mode 1)
+(set-fringe-mode 0)
 (global-display-line-numbers-mode 1)
-(setq display-line-numbers-type 'relative)
 
 (load-theme 'gruber-darker t)
 (add-to-list 'default-frame-alist '(font . "Iosevka-20"))
 
-;; C mode defaults
-(setq-default indent-tabs-mode nil)
+;;; C mode defaults
 (setq-default c-basic-offset 4)
-(setq-default tab-width 4)
-
 (add-hook 'c-mode-hook (lambda ()
                          (interactive)
                          (c-toggle-comment-style -1)))
 
-;; Dired
+;;; Dired
 (require 'dired-x)
 (setq dired-omit-files (concat dired-omit-files "\\|^\\..+$")
       dired-listing-switches "-alh"
       dired-mouse-drag-files t)
 (setq-default dired-dwim-target t)
 
-;;; backup/autosave
+;;; Backup/autosave
 (defvar backup-dir (expand-file-name "~/.emacs.d/backup/"))
 (defvar autosave-dir (expand-file-name "~/.emacs.d/autosave/"))
 (setq backup-directory-alist (list (cons ".*" backup-dir)))
 (setq auto-save-list-file-prefix autosave-dir)
 (setq auto-save-file-name-transforms `((".*" ,autosave-dir t)))
 
-(setq default-directory "")
-(add-hook 'emacs-startup-hook
-          (lambda ()
-            (dired default-directory)))
-
+;;; Move text
 (add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
 (require 'move-text)
-(global-set-key (kbd "M-<up>")   #'move-text-up)
-(global-set-key (kbd "M-<down>") #'move-text-down)
+(global-set-key (kbd "M-p") 'move-text-up)
+(global-set-key (kbd "M-n") 'move-text-down)
 
-;; Custom keybinds
+;;; Custom keybinds
 (global-set-key (kbd "C-c d") 'duplicate-line)
